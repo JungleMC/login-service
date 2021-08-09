c19b3e3115cd15071af0b66b943dcb1f94259daa
Ella Fox <ella@fox.gal>
2021-08-09 16:08:33 +0200

    Cleanup debug

--- pkg/service/java_edition.go
+++ pkg/service/java_edition.go
@@ -7,8 +7,7 @@ import (
 )
 
 func (s *LoginService) onPlayerLoginEvent(event *events.PlayerLoginEvent) error {
-	networkId, _ := uuid.FromBytes(event.NetworkId)
 	profileId, _ := uuid.FromBytes(event.ProfileId)
-	log.Printf("[Network ID: %s] [Profile ID: %s]: %s joined the game\n", networkId.String(), profileId.String(), event.Username)
+	log.Printf("[Profile ID: %v]: %v joined the game\n", profileId.String(), event.Username)
 	return nil
 }
