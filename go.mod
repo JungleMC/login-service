973d79d889728ad23e446d204b6c003b928d611c
Ella Fox <ella@fox.gal>
2021-08-09 15:35:37 +0200

    Handle PlayerLoginEvent gracefully (dummy function)

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/JungleMC/sdk v0.0.0-20210808090323-174ad2884541
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/go-redis/redis/v8 v8.11.2
-	github.com/google/uuid v1.1.2
+	github.com/google/uuid v1.3.0
 	google.golang.org/protobuf v1.27.1 // indirect
 )
 
--- pkg/service/java_edition.go
+++ pkg/service/java_edition.go
@@ -2,8 +2,13 @@ package service
 
 import (
 	"github.com/JungleMC/sdk/pkg/events"
+	"github.com/google/uuid"
+	"log"
 )
 
 func (s *LoginService) onPlayerLoginEvent(event *events.PlayerLoginEvent) error {
+	networkId, _ := uuid.FromBytes(event.NetworkId)
+	profileId, _ := uuid.FromBytes(event.ProfileId)
+	log.Printf("[Network ID: %s] [Profile ID: %s]: %s joined the game\n", networkId.String(), profileId.String(), event.Username)
 	return nil
 }
