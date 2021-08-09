eb09e55f0b664ff17335533ea7ed17b65e66fabe
Ella Fox <ella@fox.gal>
2021-08-09 13:37:02 +0200

    Redis pub/sub transport: proof of concept implementation with protobufs

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/JungleMC/login-service
 go 1.16
 
 require (
+	github.com/JungleMC/sdk v0.0.0-20210808090323-174ad2884541
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/go-redis/redis/v8 v8.11.2
-	github.com/JungleMC/sdk v0.0.0-20210808090323-174ad2884541
 	github.com/google/uuid v1.1.2
+	google.golang.org/protobuf v1.27.1 // indirect
 )
 
 replace github.com/JungleMC/sdk => ../sdk
--- pkg/service/login_service.go
+++ pkg/service/login_service.go
@@ -1,24 +1,60 @@
 package service
 
 import (
+	"context"
+	"errors"
 	"github.com/JungleMC/login-service/internal/config"
+	"github.com/JungleMC/sdk/pkg/redis/messages"
 	"github.com/caarlos0/env"
 	"github.com/go-redis/redis/v8"
+	"log"
 )
 
 var Get *LoginService
 
 type LoginService struct {
-	RDB *redis.Client
+	rdb  *redis.Client
+	channel <-chan *redis.Message
 }
 
 func Start(rdb *redis.Client) {
 	Get = &LoginService{
-		RDB: rdb,
+		rdb: rdb,
 	}
 
 	config.Get = &config.Config{}
 	if err := env.Parse(config.Get); err != nil {
 		panic(err)
 	}
+
+	Get.Bootstrap()
+}
+
+func (s *LoginService) Bootstrap() {
+	s.channel = s.rdb.PSubscribe(context.Background(), "login.*").Channel()
+	for msg := range s.channel {
+		err := s.onMessage(msg)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+}
+
+func (s *LoginService) onMessage(msg *redis.Message) error {
+	switch msg.Channel {
+	case "login.begin":
+		return s.onLoginBegin(msg)
+	}
+	return errors.New("not implemented: " + msg.Channel)
+}
+
+func (s *LoginService) onLoginBegin(m *redis.Message) error {
+	msg := &messages.LoginBegin{}
+	err := msg.UnmarshalBinary([]byte(m.Payload))
+	if err != nil {
+		return err
+	}
+	log.Println(msg.Username)
+	return nil
 }
